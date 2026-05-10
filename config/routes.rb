ecf060a5137beff14de2c8cb5242934b3f08a5fb
kylePellerin <kpellerin@bowdoin.edu>
2026-05-10 15:15:15 -0400

    setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root "cafe_app#home"
   get "cafe_app/home"
+  get "/" => "cafe_app#home"
   # test
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
